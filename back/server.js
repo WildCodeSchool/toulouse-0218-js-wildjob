b1f3325c5f46e58bb36d08a71b4f41f83a64566a
Romain Vaur <romainwcs@gmail.com>
2018-04-18 10:21:14 +0200

    création dossier back

---
+++ back/server.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const app = express()
+
+app.use(express.static(__dirname))
+
+const indexHtml = /* @html */ `
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+  <title>Wild Job</title>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+    crossorigin="anonymous" />
+  <link rel="stylesheet" href="style.css" />
+  <link href="https://fonts.googleapis.com/css?family=Paytone+One" rel="stylesheet">
+</head>
+
+<body class="payTone">
+  <div id="main"></div>
+
+  <!-- script google maps cle API -->
+  <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCwC__7psOPTWbszU21xZvnsFL2XdrrpZk "></script>
+  <!-- script google maps cle API end -->
+
+  <!-- script dynamique -->
+  <script src="page.js"></script>
+  <script src="app.js"></script>
+  <!-- script dynamique end -->
+
+  <!--script bootstrap-->
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  <!--script bootstrap end-->
+</body>
+
+</html>`
+
+app.get('*', (req, res) => {
+  res.send(indexHtml)
+})
+
+app.listen(3000)
